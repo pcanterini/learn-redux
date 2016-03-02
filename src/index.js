df4fd98443659ace9a8f1e1f78c2c78c4905e49c
Pedro Canterini <pedro@pcanterini.com>
2016-03-02 12:44:14 -0800

    add api call and video components

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import YTSearch from 'youtube-api-search';
 
-const App = () => <div>App</div>;
+const API_KEY = 'AIzaSyDFxQSWqI6btiMIbvWjR6496lgVOcW3bAk';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: []
+    }
+    YTSearch({key: API_KEY, term: 'snakes'}, videos => {
+      this.setState({videos});
+    });
+  }
+  render() {
+    return(
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+};
 
 ReactDOM.render(<App />, document.querySelector('.app'));
